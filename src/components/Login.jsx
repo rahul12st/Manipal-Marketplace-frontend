d2242bfb2eb1e2fa872da7f4e8d33d91f00d40e0
Rahul Singh <96829896+rahul12st@users.noreply.github.com>
2024-09-18 22:27:03 +0530

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -5,59 +5,67 @@ import axios from "axios";
 import API_URL from "../constants";
 
 function Login() {
-    const navigate = useNavigate()
+    const navigate = useNavigate();
 
     const [username, setusername] = useState('');
     const [password, setpassword] = useState('');
 
     const handleApi = () => {
-        console.log("Login button clicked!"); // Add this log to check if the function is triggered
+        console.log("Login button clicked!");  // Verify if this is logged
 
-    if (!username || !password) {
-        alert('Username and password are required');
-        return;
-    }
+        if (!username || !password) {
+            alert('Username and password are required');
+            return;
+        }
+
         const url = API_URL + '/login';
         const data = { username, password };
+
+        console.log("Sending request to:", url, "with data:", data);  // Log the request data
+
         axios.post(url, data)
-            .then((res) => {
-                if (res.data.message) {
-                    if (res.data.token) {
-                        localStorage.setItem('token', res.data.token);
-                        localStorage.setItem('userId', res.data.userId);
-                         console.log("Navigating to home...");
-                        navigate('/');
-                    }
-                }
-            })
-            .catch((err) => {
-                alert('SERVER ERR')
-            })
-    }
+        .then((res) => {
+            console.log("Response from server:", res);  // Log the full response
+            if (res.data.message && res.data.token) {
+                localStorage.setItem('token', res.data.token);
+                localStorage.setItem('userId', res.data.userId);
+                console.log("Login successful, navigating to home...");
+                navigate('/');
+            } else {
+                console.log("Login failed:", res.data.message);
+            }
+        })
+        .catch((err) => {
+            alert('SERVER ERR');
+            console.error("Login Error:", err);
+        });
+    };
 
     return (
         <div>
             <Header />
             <div className="p-3 m-3">
-                <h3> Welocme to Login Page </h3>
-                <br></br>
+                <h3> Welcome to Login Page </h3>
+                <br />
                 USERNAME
                 <input className="form-control" type="text" value={username}
                     onChange={(e) => {
-                        setusername(e.target.value)
+                        setusername(e.target.value);
+                        console.log("Username:", e.target.value);  // Debugging log
                     }} />
-                <br></br>
+                <br />
                 PASSWORD
-                <input className="form-control" type="text" value={password}
+                <input className="form-control" type="password" value={password}
                     onChange={(e) => {
-                        setpassword(e.target.value)
+                        setpassword(e.target.value);
+                        console.log("Password:", e.target.value);  // Debugging log
                     }} />
-                <br></br>
+                <br />
                 <button className="btn btn-primary mr-3" onClick={handleApi}> LOGIN </button>
-                <Link className="m-3" to="/signup">  SIGNUP </Link>
+                <Link className="m-3" to="/signup"> SIGNUP </Link>
             </div>
         </div>
-    )
+    );
 }
 
 export default Login;
