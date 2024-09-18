f4c81dd26e05e100a02f1c3ebf464982dc25948f
Rahul Singh <96829896+rahul12st@users.noreply.github.com>
2024-09-18 22:09:51 +0530

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,7 @@
 
 const API_URL = process.env.NODE_ENV != 'development' ?
-    'https://manipal-marketplace-backend-dp7l.vercel.app/' :
-    'https://manipal-marketplace-backend-dp7l.vercel.app/';
+    'https://manipal-marketplace-backend-dp7l.vercel.app' :
+    'https://manipal-marketplace-backend-dp7l.vercel.app';
 
 console.log(process.env, "API_URL");
 
